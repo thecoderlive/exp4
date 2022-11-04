5e7e23699f1eb824cc345e8091cfbd01e679504e
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-04 23:57:33 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "706ad87f-aa3b-4a65-aa99-65d59bf28367",
+    "home_feed": [
+        {
+            "id": "db3f9b13-83a9-41dc-9a67-e0a3e93ba814",
+            "artist_name": "company",
+            "home_carousel": [
+                {
+                    "id": "1d201ba1-8f94-4ac1-8271-e631bdc1997c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+                },
+                {
+                    "id": "636d0ef6-c79a-4a96-b7d9-eb8c9df92286",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png"
+                },
+                {
+                    "id": "0f70f758-083e-4466-ab82-13e8c9a315d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png"
+                },
+                {
+                    "id": "2698ac8c-9718-4ac9-ac82-b76e26550aab",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                },
+                {
+                    "id": "ef2ef655-9aa4-4300-ba1b-60691f7483db",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                }
+            ],
+            "art_caption": "over",
+            "art_description": "Behavior book too economic care perhaps mean. Pick she argue never soldier key. Floor evidence light TV glass treatment some."
+        },
+        {
+            "id": "d1a36dd8-2fa5-41a6-8c6f-c66087f2ba34",
+            "artist_name": "parent",
+            "home_carousel": [
+                {
+                    "id": "0668ec0a-3b1f-4bd4-ac1f-f0383334e542",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png"
+                },
+                {
+                    "id": "4a339717-6e25-498d-b292-b90806b3d121",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                },
+                {
+                    "id": "64422619-5bdf-4b90-900b-b66d70e3e25b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                },
+                {
+                    "id": "4d7f4da4-8025-4f61-829d-32292f25fe47",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/295.png"
+                },
+                {
+                    "id": "9ee303a6-3921-4d14-8a97-c940f7f16275",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/193.png"
+                }
+            ],
+            "art_caption": "my",
+            "art_description": "Price decision say take. Road wonder check."
+        },
+        {
+            "id": "7b6ed728-9b1e-4510-b25f-1f2cc72f0137",
+            "artist_name": "space",
+            "home_carousel": [
+                {
+                    "id": "2c7d69d9-6947-4e3a-8827-7fc52060ab4c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/168.png"
+                },
+                {
+                    "id": "3485b5bf-9d82-443e-8b70-dc8bd04880fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/136.png"
+                },
+                {
+                    "id": "2410271d-2488-422a-89d0-4f7a793aebb5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png"
+                },
+                {
+                    "id": "2c0ca5eb-915d-4d59-918b-e6a96c8c706e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/168.png"
+                },
+                {
+                    "id": "830ec3c4-a00f-43a9-ad77-83f6f57504c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png"
+                }
+            ],
+            "art_caption": "will",
+            "art_description": "Create try crime article. Line easy once term act."
+        },
+        {
+            "id": "5a7b8474-f729-42d3-bca7-5219ef10820c",
+            "artist_name": "Republican",
+            "home_carousel": [
+                {
+                    "id": "3f8069ce-d4c2-46cf-8174-38204ea813e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/242.png"
+                },
+                {
+                    "id": "625c4023-42e7-4b4d-8620-e95a8b440613",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                },
+                {
+                    "id": "7095164b-1d42-44fe-b3b5-935521b08ea1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/358.png"
+                },
+                {
+                    "id": "7d5e01f5-d2fa-4edd-9fb9-400a559d043c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "5bfa175d-3b62-4f37-8f07-d84cd3478a29",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/294.png"
+                }
+            ],
+            "art_caption": "truth",
+            "art_description": "Down land many sing international movie stop. Hope official everyone little pull debate probably I."
+        },
+        {
+            "id": "16d0aa8d-796e-451d-a007-76b5559561a2",
+            "artist_name": "last",
+            "home_carousel": [
+                {
+                    "id": "71177e18-c04a-458d-8cc7-9f98f172edf5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                },
+                {
+                    "id": "0dddb90e-d489-40bb-b622-3f83d917a6ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                },
+                {
+                    "id": "3f6ae9a1-6fbc-49a9-a85b-837244198efb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "b5448dbd-3c23-4509-ac4e-3f6b421be542",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png"
+                },
+                {
+                    "id": "994f4549-2acb-4845-b0d0-c5dbdd91a368",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/72.png"
+                }
+            ],
+            "art_caption": "about",
+            "art_description": "Card bill remain if. Unit own since as."
+        }
+    ]
+}
